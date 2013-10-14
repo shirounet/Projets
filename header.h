ab1be0c7227502ade54f95e7f4b587ae6692fff6
AiraneToufik <tf.airane@gmail.com>
2013-10-15 00:27:47 +0200

    15/10/13 00:30
    Toutes les fonctions sont : OK
    Faire l'affichage et revoir tri par prenom

--- header.h
+++ header.h
@@ -1,14 +1,35 @@
+	/*					~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~					*/
+	//																			//
+	//					DEBUT	-	CONSTANTE D'AFFICHAGE                       //
+	//																			//
+	/*					~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~					*/
+	
+#define WELCOME "[~] Welcome , select option :\n" \
+"[~] Des mises a jours :\n(I)\tInscrire\n(S)\tSupprimer\n(M)\tModifier\n(Q)\tQuitter\n\n[~] "
+
+	/*					~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~					*/
+	//																			//
+	//					FIN    	-	CONSTANTE D'AFFICHAGE                       //
+	//																			//
+	/*					~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~					*/
     struct Personne {
            char nom[16];
            char prenom[16];
            char tel[11];
            int cat;
            };
+           
+    typedef struct Personne Personne;
 
-    void inscription(struct Personne*, int*, int*, int*);
-    void supprimer(struct Personne*, int*, int*, int*);
-    void modification(struct Personne*, int *, int*, int*);
-    void affichage(struct Personne*, int);
-    void affichagepro(struct Personne *, int, int*, int);
-    int recherche(struct Personne *, int);
-    int tri(struct Personne *, int, int *, int);
+    void inscription(Personne*, int*, int*, int*);
+    void supprimer(Personne*, int*, int*, int*);
+    void modification(Personne*, int *, int*, int*);
+    void affichage(Personne*, int);
+    void affichageperso(Personne *, int , int *, int);
+    void affichagepro(Personne *, int, int*, int);
+    int recherche(Personne *, int);
+
+    void rech_num(Personne *, int, int *, int);
+    void rech_num_by_name(Personne *, int , int *, int );
+    void rech_name(Personne *, int , int *, int );
+    void rech_char(Personne *repertoire, int nbrperso, int *pro, int nbrpro);
--- main.c
+++ main.c
@@ -3,61 +3,94 @@
 #include <string.h>
 #include "header.h"
 
-int main() {
+int main() {			// DEBUT main()
+
 	/*					~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~					*/
 	//																			//
 	//					DEBUT	-	DECLARATION DES VARIABLES					//
 	//																			//
 	/*					~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~					*/
 	
-    struct Personne repertoire[100] = { 0 };	// repertoire de 100 personnes
-    int pro[100];								// tableau de 100 professionels
+    Personne repertoire[100] = { 0 };	// repertoire de 100 personnes
+    int pro[100] = { 0 };						// tableau de 100 professionels
     int nbrperso = 0;							// Le nombre de personne(s) existante(s)
     int nbrpro = 0;								// Le nombre de personne(s) professionelle(s)
-    char code;									// code de transaction
-
-    printf("[~] Hi, please enter code:\n"); // Bonjour
-    code=getchar();
-    while(code!='Q' && code!='q') {
+    char code;								 // code de transaction
+	
+	/*					~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~					*/
+	//																			//
+	//					FIN		-	DECLARATION DES VARIABLES					//
+	//																			//
+	/*					~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~					*/
+	
+	
+	/*					~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~					*/
+	//																			//
+	//					DEBUT	-		CODE DE TRANSACTION						//
+	//																			//
+	/*					~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~					*/
+	
+    printf(WELCOME);
+    
+    code=getchar();		// STDIN <code>
+    while(code!='Q' && code!='q') {				// Option 'Q' ou 'q' pour quitter le programme
                     switch(code) {
-                                 case 'I':
+                                 case 'I':		// Option 'I' ou 'i' pour inscription()
+								 case 'i':
                                      inscription(repertoire, &nbrperso, pro, &nbrpro);
                                       break;
-                                 case 'S':
+                                 case 'S':		// Option 'S' ou 's' pour supprimer()
+								 case 's':
                                      supprimer(repertoire, &nbrperso, pro, &nbrpro);
                                         break;
-                                 case 'M':
+                                 case 'M':		// Option 'M' ou 'm' pour modifier()
+								 case 'm':
                                      modification(repertoire, &nbrperso, pro, &nbrpro);
                                       break;
-                                 case 'T':
+                                 case 'T':		// Option 'T' ou 't' pour afficher tous le repertoire
+								 case 't':
                                      affichage(repertoire, nbrperso);
                                         break;
-                                 case 'A':
-                                     affichagepro(repertoire, nbrperso, pro, nbrpro);
+                                 case 'A':		// Option 'A' ou 'a' pour afficher uniquement les personnels
+								 case 'a':
+                                     affichageperso(repertoire, nbrperso, pro, nbrpro);
                                       break;
-                                 case 'P':
+                                 case 'P':     // Option 'A' ou 'a' pour afficher uniquement les professionels
+                                 case 'p':
+                                      affichagepro(repertoire, nbrperso, pro, nbrpro);
                                         break;
                                  case 'R':
+                                 case 'r':
+                                      rech_num(repertoire, nbrperso, pro, nbrpro); // Option 'R' recherche numero par (nom) ou (nom et prenom)
                                       break;
                                  case 'N':
+                                 case 'n':
+                                      rech_num_by_name(repertoire, nbrperso, pro, nbrpro); // Option 'N' recherche numero par nom
                                         break;
                                  case 'B':
+                                 case 'b':
+                                      rech_name(repertoire, nbrperso, pro, nbrpro); // Option 'B' recherche par numero de telephone
                                       break;
                                  case 'L':
+                                 case 'l':
+                                      rech_char(repertoire, nbrperso, pro, nbrpro);
                                         break;
                                  case 'C':
                                      system("cls");
                                         break;
                                  default:
-                                 printf("[#] Error : Code Unknow\n"); // Code inconnu
+                                 printf("[#] Error : Code Unknow\n"); // Si le code de transaction est inconnu
                                  }
-    printf("[#]Le repertoire contient %d personnes dont %d pro\n[~] Please enter code:\n", nbrperso, nbrpro); // Bonjour
+								 
+    printf("[#] Le repertoire contient %d personnes dont %d pro\n[~] Please enter code:\n\n", nbrperso, nbrpro);
     fflush(stdin);
-    code=getchar();
+    code=getchar();		// STDIN <code>
     }
-    printf("[0] Bye Bye :)\n");
+	
+    printf("Bye Bye :)\n");
+	system("pause");
     return 0;
-}
+}						// Fin main()
 
 void inscription(struct Personne *repertoire, int *nbrperso, int *pro, int *nbrpro) {
     printf("[~] Enter <last name> :\n");
@@ -85,9 +118,11 @@ void inscription(struct Personne *repertoire, int *nbrperso, int *pro, int *nbrp
             scanf("%d",&(repertoire[*nbrperso].cat));
         }
 
-        int i;                                   // ON TRI LE repertoire
+        int i;
         struct Personne temp;
-        for(i=0;i<*nbrperso && (strcmp(repertoire[*nbrperso].nom, repertoire[i].nom)!=-1);i++);
+        for(i=0;i<*nbrperso && ((strcmp(repertoire[*nbrperso].nom, repertoire[i].nom)!=-1));i++) {
+        for(i;i<*nbrperso && ((strcmp(repertoire[*nbrperso].prenom, repertoire[i].prenom)!=-1));i++); //A VERIFIER TRI PRENOM
+        }
         for(i;i<*nbrperso;i++) {
         temp=repertoire[i];
         repertoire[i]=repertoire[*nbrperso];
@@ -162,7 +197,7 @@ void modification(struct Personne* repertoire, int *nbrperso, int *pro, int *nbr
     printf("[~] Enter <first name> :\n");
     fflush(stdin);
     gets(repertoire[*nbrperso].prenom);
-
+    
     int rech=recherche(repertoire, *nbrperso);
 
     if(rech>-1) {
@@ -173,7 +208,7 @@ void modification(struct Personne* repertoire, int *nbrperso, int *pro, int *nbr
                     printf("[~] Enter NEW <last name> :\n");
                     fflush(stdin);
                     gets(repertoire[*nbrperso].nom);
-
+    
                     printf("[~] Enter NEW <first name> :\n");
                     fflush(stdin);
                     gets(repertoire[*nbrperso].prenom);
@@ -240,31 +275,208 @@ int recherche(struct Personne *repertoire, int nbrperso) {
     return -1; // Return -1 if Contact Unknow
 }
 
-int tri(struct Personne *repertoire, int nbrperso, int *pro, int nbrpro) // OBSOLETE
-{
-    int i, ii;
-    struct Personne temp;
-        for(i=0;i<nbrperso && (strcmp(repertoire[nbrperso].nom, repertoire[i].nom)!=-1);i++);
-    ii=i;
-        for(i;i<nbrperso;i++) {
-        temp=repertoire[i];
-        repertoire[i]=repertoire[nbrperso];
-        repertoire[nbrperso]=temp;
-        }
-    return ii;
-}
-
 void affichage(struct Personne *repertoire, int nbrperso)
 {
-    int i;
+    int i, pause=0;
     for(i=0;i<nbrperso;i++) {
+                            if((++pause)%6==0)
+                            {
+                                       system("pause");
+                            }
         printf("Last Name :%s\nFirst Name :%s\nTelephone Number :%s\nCat : %d\n----------\n", repertoire[i].nom,repertoire[i].prenom,repertoire[i].tel,repertoire[i].cat);
     }
 }
 
+void affichageperso(struct Personne *repertoire, int nbrperso, int *pro, int nbrpro) {
+    int i, pause=0;
+    for(i=0;i<nbrperso;i++) {
+    if(!(repertoire[i].cat)) {
+                            if((++pause)%6==0)
+                            {
+                                       system("pause");
+                            }
+            printf("Last Name :%s\nFirst Name :%s\nTelephone Number :%s\nCat : %d\n----------\n", repertoire[i].nom,repertoire[i].prenom,repertoire[i].tel,repertoire[i].cat);
+            }
+    }
+}
+
 void affichagepro(struct Personne *repertoire, int nbrperso, int *pro, int nbrpro) {
-    int i;
+    int i, pause=0;
     for(i=0;i<nbrpro;i++) {
+                           if((++pause)%6==0)
+                            {
+                                       system("pause");
+                            }
             printf("Last Name :%s\nFirst Name :%s\nTelephone Number :%s\nCat : %d\n----------\n", repertoire[pro[i]].nom,repertoire[pro[i]].prenom,repertoire[pro[i]].tel,repertoire[pro[i]].cat);
     }
+}
+
+void rech_num(struct Personne *repertoire, int nbrperso, int *pro, int nbrpro) {
+     int r, i, pause=0;
+     struct Personne temp;
+     
+     do{
+     printf("Effectuer la recherche uniquement sur les pro ?\n(1) Oui\n(0) Non, sur tout le repertoire\n");
+     fflush(stdin);
+     scanf("%d",&r);
+     }while((r!=0) && (r!=1));
+     
+                   printf("[~] Enter <last name> :\n");
+                   fflush(stdin);
+                   gets(temp.nom);
+                   
+                   printf("[~] Enter <first name> :\n");
+                   fflush(stdin);
+                   gets(temp.prenom);
+                   
+     if(r)
+     {
+          for(i=0; i<nbrpro && (strcmp(temp.nom,repertoire[pro[i]].nom)!=1);i++) {
+                  if(strcmp(temp.nom,repertoire[pro[i]].nom)==0) {
+                  if(strcmp(temp.prenom,repertoire[pro[i]].prenom)==0 || !strlen(temp.prenom)) {
+                                                                      if((++pause)%6==0)
+                            {
+                                       system("pause");
+                            }
+                  printf("Last Name :%s\nFirst Name :%s\nTelephone Number :%s\nCat : %d\n----------\n", repertoire[pro[i]].nom,repertoire[pro[i]].prenom,repertoire[pro[i]].tel,repertoire[pro[i]].cat);
+                  }
+                  }
+          }
+     }
+     else
+     {
+         for(i=0; i<nbrperso && (strcmp(temp.nom,repertoire[i].nom)!=1);i++) {
+                  if(strcmp(temp.nom,repertoire[i].nom)==0) {
+                  if(strcmp(temp.prenom,repertoire[i].prenom)==0 || !strlen(temp.prenom)) {
+                                                                                             if((++pause)%6==0)
+                            {
+                                       system("pause");
+                            }
+                  printf("Last Name :%s\nFirst Name :%s\nTelephone Number :%s\nCat : %d\n----------\n", repertoire[i].nom,repertoire[i].prenom,repertoire[i].tel,repertoire[i].cat);
+                  }
+                  }
+         }
+     }
+}
+
+void rech_num_by_name(struct Personne *repertoire, int nbrperso, int *pro, int nbrpro) {
+     int r, i, pause=0;
+     struct Personne temp;
+     
+     do{
+     printf("Effectuer la recherche uniquement sur les pro ?\n(1) Oui\n(0) Non, sur tout le repertoire\n");
+     fflush(stdin);
+     scanf("%d",&r);
+     }while((r!=0) && (r!=1));
+     
+                   printf("[~] Enter <last name> :\n");
+                   fflush(stdin);
+                   gets(temp.nom);
+                   
+                   printf("[~] Enter <first name> :\n");
+                   fflush(stdin);
+                   gets(temp.prenom);
+                   
+     if(r)
+     {
+          for(i=0; i<nbrpro && (strcmp(temp.nom,repertoire[pro[i]].nom)!=1);i++) {
+                  if(strcmp(temp.nom,repertoire[pro[i]].nom)==0) {
+                                                                                             if((++pause)%6==0)
+                            {
+                                       system("pause");
+                            }
+                  printf("Last Name :%s\nFirst Name :%s\nTelephone Number :%s\nCat : %d\n----------\n", repertoire[pro[i]].nom,repertoire[pro[i]].prenom,repertoire[pro[i]].tel,repertoire[pro[i]].cat);
+                  }
+          }
+     }
+     else
+     {
+         for(i=0; i<nbrperso && (strcmp(temp.nom,repertoire[i].nom)!=1);i++) {
+                  if(strcmp(temp.nom,repertoire[i].nom)==0) {
+                                                                                        if((++pause)%6==0)
+                            {
+                                       system("pause");
+                            }
+                  printf("Last Name :%s\nFirst Name :%s\nTelephone Number :%s\nCat : %d\n----------\n", repertoire[i].nom,repertoire[i].prenom,repertoire[i].tel,repertoire[i].cat);
+                  }
+         }
+     }
+}
+
+void rech_name(struct Personne *repertoire, int nbrperso, int *pro, int nbrpro) {
+     int r, i, pause=0;
+     struct Personne temp;
+     
+     do{
+     printf("Effectuer la recherche uniquement sur les pro ?\n(1) Oui\n(0) Non, sur tout le repertoire\n");
+     fflush(stdin);
+     scanf("%d",&r);
+     }while((r!=0) && (r!=1));
+     
+                   printf("[~] Enter <telephone number> :\n");
+                   fflush(stdin);
+                   gets(temp.tel);
+                   
+     if(r)
+     {
+          for(i=0; i<nbrpro && (strcmp(temp.tel,repertoire[pro[i]].tel)!=0);i++);
+                  if(i!=nbrpro) {
+                                                            if((++pause)%6==0)
+                            {
+                                       system("pause");
+                            }
+                  printf("Last Name :%s\nFirst Name :%s\nTelephone Number :%s\nCat : %d\n----------\n", repertoire[pro[i]].nom,repertoire[pro[i]].prenom,repertoire[pro[i]].tel,repertoire[pro[i]].cat);
+                  }
+     }
+     else
+     {
+         for(i=0; i<nbrperso && (strcmp(temp.tel,repertoire[i].tel)!=0);i++);
+                  if(i!=nbrperso) {
+                                                              if((++pause)%6==0)
+                            {
+                                       system("pause");
+                            }
+                  printf("Last Name :%s\nFirst Name :%s\nTelephone Number :%s\nCat : %d\n----------\n", repertoire[i].nom,repertoire[i].prenom,repertoire[i].tel,repertoire[i].cat);
+                  }
+     }
+}
+
+void rech_char(struct Personne *repertoire, int nbrperso, int *pro, int nbrpro) {
+     int r, i, pause=0;
+     char u;
+     
+     do{
+     printf("Effectuer la recherche uniquement sur les pro ?\n(1) Oui\n(0) Non, sur tout le repertoire\n");
+     fflush(stdin);
+     scanf("%d",&r);
+     }while((r!=0) && (r!=1));
+     
+                   printf("[~] Enter <Lettre> :\n");
+                   fflush(stdin);
+                   u=getchar();
+                   
+     if(r)
+     {
+          for(i=0; i<nbrpro && (u <= repertoire[pro[i]].nom[0]) ;i++) {
+                  if(u == repertoire[pro[i]].nom[0]) {
+                                                   if((++pause)%6==0)
+                            {
+                                       system("pause");
+                            }
+                  printf("Last Name :%s\nFirst Name :%s\nTelephone Number :%s\nCat : %d\n----------\n", repertoire[pro[i]].nom,repertoire[pro[i]].prenom,repertoire[pro[i]].tel,repertoire[pro[i]].cat);
+                  }
+                  }
+     }
+     else
+     {
+         for(i=0; i<nbrperso && (u <= repertoire[i].nom[0]) ;i++) {
+                  if(u == repertoire[i].nom[0]) {
+                                                              if((++pause)%6==0)
+                            {
+                                       system("pause");
+                            }
+                  printf("Last Name :%s\nFirst Name :%s\nTelephone Number :%s\nCat : %d\n----------\n", repertoire[i].nom,repertoire[i].prenom,repertoire[i].tel,repertoire[i].cat);
+                  }
+                  }
+     }
 }
